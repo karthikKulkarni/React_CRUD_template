697263b5b48b487a833ee91d5d90f472e2d759f3
kk <karthik.k8687@gmail.com>
2020-02-14 14:19:56 +0530

    Adding Detailes page

--- src/screens/DetailedPost/DetailedPostPage.tsx
+++ src/screens/DetailedPost/DetailedPostPage.tsx
@@ -1,5 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import { Post } from '../../store/Posts/types';
+import styled from 'styled-components';
 
-export default function DetailedPostPage() {
-  return <div>Detailed post component</div>;
+export interface Props extends RouteComponentProps<any>, React.Props<any> {}
+
+const Container = styled.div`
+  height: 100vh;
+  padding: 1rem 1rem;
+  background: #3e4e50;
+`;
+
+const PostHeader = styled.h2`
+  text-align: left;
+  padding: 1rem;
+  background: #f2aa7e;
+  border-radius: 5px;
+`;
+
+const PostBody = styled.p`
+  text-align: left;
+  text-indent: 2em;
+  margin-top: 2em;
+  background: #f3c969;
+  border-radius: 5px;
+`;
+
+class DetailedPostPage extends Component<Props> {
+  render() {
+    // @ts-ignore
+    const { post }: Post = this.props.location.state;
+    return (
+      <Container className="details_container">
+        <PostHeader>{post.title}</PostHeader>
+        <PostBody>{post.body}</PostBody>
+      </Container>
+    );
+  }
 }
+
+export default DetailedPostPage;
--- src/screens/Landing/LandingPage.tsx
+++ src/screens/Landing/LandingPage.tsx
@@ -11,20 +11,19 @@ import { Post, UpdatePostInput, DeletePostInput, AllPostsData } from '../../stor
 import { StandardApiState, GlobalState } from '../../store';
 import { Button } from '../../components/Button';
 import { ListContainer, RowItem } from '../../components/List';
-import { Link, Redirect } from 'react-router-dom';
+import { Redirect, RouteComponentProps } from 'react-router-dom';
 
 const Container = styled.div`
   height: 100%;
   width: 100%;
 `;
 
-export interface Props {
+export interface Props extends RouteComponentProps {
   allPostsRequest?: typeof allPostsRequest;
   rawPosts?: StandardApiState<AllPostsData>;
   addPostsRequest?: typeof addPostsRequest;
   updatePostRequest?: typeof updatePostRequest;
   deletePostRequest?: typeof deletePostRequest;
-
   posts: Post[];
 }
 
@@ -52,17 +51,15 @@ export class __LandingPage extends Component<Props> {
     this.props.deletePostRequest && this.props.deletePostRequest(deletePost);
   };
 
-  handleOnClick = () => {
-    // some action...
-    // then redirect
-    this.setState({ redirect: '/detailedPost' });
+  handleOnClick = (item: Post) => {
+    this.props.history.push('/detailedPost', { post: item });
   };
 
   render() {
     const { posts } = this.props;
 
     if (this.state.redirect !== '/') {
-      return <Redirect push to={this.state.redirect} />;
+      return <Redirect push to={{ pathname: this.state.redirect, state: { title: 'Bla' } }} />;
     }
 
     return (
@@ -70,7 +67,10 @@ export class __LandingPage extends Component<Props> {
         <Button>Normal Button</Button>
         <Button primary={true}>Primary Button</Button>
         <ListContainer className="list_container">
-          {posts && posts.map(item => <RowItem onClick={() => this.handleOnClick()}>{item.title}</RowItem>)}
+          {posts &&
+            posts.map((item: Post) => (
+              <RowItem onClick={() => this.handleOnClick(item)}>{item.title}</RowItem>
+            ))}
         </ListContainer>
       </Container>
     );
